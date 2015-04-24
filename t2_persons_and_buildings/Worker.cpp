66991817f249d3e9616b400120e8a321ba44ac47
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-04-24 19:23:25 +0300

    t2_persons_and_buildings: CWorker now works with raw data

--- t2_persons_and_buildings/Worker.cpp
+++ t2_persons_and_buildings/Worker.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "Worker.h"
+#include "RawDataUtils.h"
 
 using namespace std;
 
@@ -9,6 +10,20 @@ CWorker::CWorker(Gender gender, unsigned age, string const& name, unsigned heigh
 	,m_company(company)
 	,m_specialty(specialty)
 {}
+
+CWorker::CWorker(istream &in,
+	function<CCompany const*(string const& name)> getCompanyByName)
+	:CPerson(in)
+	,m_company(getCompanyByName(RawData::ReadString(in)))
+	,m_specialty(RawData::ReadString(in))
+{}
+
+void CWorker::WriteRawData(std::ostream &out) const
+{
+	CPerson::WriteRawData(out);
+	RawData::WriteString(m_company->GetName(), out);
+	RawData::WriteString(m_specialty, out);
+}
 
 CCompany const* CWorker::GetCompany() const
 {
--- t2_persons_and_buildings/Worker.h
+++ t2_persons_and_buildings/Worker.h
@@ -10,6 +10,11 @@ public:
 	CWorker(Gender gender, unsigned age, std::string const& name, unsigned height,
 		unsigned weight, CCompany const* company, std::string const& specialty);
 
+	CWorker(std::istream &in,
+		std::function<CCompany const*(std::string const& name)> getCompanyByName);
+
+	void WriteRawData(std::ostream &out) const override;
+
 	CCompany const* GetCompany() const;
 	std::string GetSpecialty() const;
 
